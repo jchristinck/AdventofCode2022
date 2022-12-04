6c0aa2a5fb5797f02a0842b99f0cb63525ef161a
waynetraub3 <justus.christinck@gmx.de>
2022-12-04 19:23:39 +0100

    day04 polish

--- day04/day04.py
+++ day04/day04.py
@@ -1,7 +1,7 @@
-import pathlib
+from pathlib import Path
 
 
 if __name__ == "__main__":
-    input = [list(map(int, row.replace('-', ',').split(','))) for row in list(filter(None, pathlib.Path("input.txt").read_text().split('\n')))]
+    input = [list(map(int, row.replace('-', ',').split(','))) for row in Path("input.txt").read_text().splitlines()]
     print(sum((row[3] - row[1]) * (row[2] - row[0]) < 1 for row in input))
     print(sum(len(list(set(range(row[0], row[1] + 1)) & set(range(row[2], row[3] + 1)))) > 0 for row in input))
